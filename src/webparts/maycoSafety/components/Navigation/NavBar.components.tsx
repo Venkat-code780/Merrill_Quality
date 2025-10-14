8e59fd7e1a74d044a98607856b4c448da76f9950
GaneshKoppisetti <ganeshkoppisetti19@gmail.com>
2025-10-14 11:41:49 +0530

    navigation toggling updated

--- src/webparts/maycoSafety/components/Navigation/NavBar.components.tsx
+++ src/webparts/maycoSafety/components/Navigation/NavBar.components.tsx
@@ -1,7 +1,7 @@
-import { faBars, faChevronDown, faChevronUp, faHome,faCogs,faFileAlt,faEye} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faChevronDown, faChevronUp, faHome, faCogs, faFileAlt, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import "../CSS/left-nav.css";
 
 export interface NavBarProps {
@@ -39,7 +39,7 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
         this.setState({ showSidebar: !prevShowSidebar })
     }
 
-    private toggleSideBarItem = (event:any,title: string) => {
+    private toggleSideBarItem = (event: any, title: string) => {
         event.preventDefault();
         let navLinks = document.querySelectorAll('.sidebar-title');
         if (navLinks.length > 0) {
@@ -48,20 +48,53 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
             });
         }
         event.currentTarget.className = 'sidebar-title left-nav-active';
-        this.setState((prevState: any) => ({
-            openSideBars: {
-                ...prevState.openSideBars,
-                [title]: !prevState.openSideBars[title]
+
+        let newOpenSideBars: { [key: string]: boolean } = {};
+        let prevOpenSideBars = { ...this.state.openSideBars };
+        // Iterate over the previous open state openSideBars
+        for (const key in prevOpenSideBars) {
+            if (prevOpenSideBars.hasOwnProperty(key)) {
+                if (key !== title) {
+                    newOpenSideBars[key] = false;
+                } else {
+                    newOpenSideBars[key] = !prevOpenSideBars[title];
+                }
             }
-        }));
+        }
+        if (!prevOpenSideBars.hasOwnProperty(title)) {
+            newOpenSideBars[title] = true; // Open it if it was not present before
+        }
+        this.setState({ openSideBars: newOpenSideBars });
+        // this.setState((prevState: any) => ({
+        //     openSideBars: {
+        //         ...prevState.openSideBars,
+        //         [title]: !prevState.openSideBars[title]
+        //     }
+        // }));
     }
     private toggleMasterItem = (title: string) => {
-        this.setState((prevState: any) => ({
-            openMasters: {
-                ...prevState.openMasters,
-                [title]: !prevState.openMasters[title]
+        let newopenMasters: { [key: string]: boolean } = {};
+        let prevopenMasters = { ...this.state.openMasters };
+        // Iterate over the previous open state openMasters
+        for (const key in prevopenMasters) {
+            if (prevopenMasters.hasOwnProperty(key)) {
+                if (key !== title) {
+                    newopenMasters[key] = false;
+                } else {
+                    newopenMasters[key] = !prevopenMasters[title];
+                }
             }
-        }));
+        }
+        if (!prevopenMasters.hasOwnProperty(title)) {
+            newopenMasters[title] = true; // Open it if it was not present before
+        }
+        this.setState({ openMasters: newopenMasters });
+        // this.setState((prevState: any) => ({
+        //     openMasters: {
+        //         ...prevState.openMasters,
+        //         [title]: !prevState.openMasters[title]
+        //     }
+        // }));
     };
 
     public render() {
@@ -73,7 +106,7 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
             'JSRA': ['JSRA Categories', 'JSRA Sub - Categories', 'PPE Types', 'JSRA Details'],
             'Unsafe Act': ['UA Types', 'UA Sub - Types']
         };
-        const FormAndViewTitles = ['SEWO', 'UCAN','SMAT','EHS','JSRA','TAG','CHECK - LIST STEP 1', 'CHECK - LIST STEP 2', 'CHECK - LIST STEP 3'];
+        const FormAndViewTitles = ['SEWO', 'UCAN', 'SMAT', 'EHS', 'JSRA', 'TAG', 'CHECK - LIST STEP 1', 'CHECK - LIST STEP 2', 'CHECK - LIST STEP 3'];
         return (
             <React.Fragment>
 
@@ -87,16 +120,16 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
                         <div className="sidebar">
                             <div>
                                 <ul className="list-unstyled ul-leftnav components mb-5">
-                                    <li id="liHome" className="mb-1" onClick={(event) => this.onNavItemClick(event)}>
-                                        <div className="sidebar-title a-home">
+                                    <li id="liHome" className="mb-1">
+                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e, 'Home')}>
                                             <NavLink to={"/Home"}><span><FontAwesomeIcon icon={faHome} /> Home</span></NavLink>
                                         </div>
                                     </li>
                                     {/* Masters Section */}
-                                    <li className="liMasters mb-2">
-                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e,'Masters')}>
+                                    <li className="liMasters mb-1">
+                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e, 'Masters')}>
                                             <span><FontAwesomeIcon icon={faCogs}></FontAwesomeIcon>
-                                            Masters
+                                                Masters
                                             </span>
                                             <span className="icon-down">
                                                 <FontAwesomeIcon icon={this.state.openSideBars['Masters'] ? faChevronUp : faChevronDown} />
@@ -118,7 +151,7 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
                                                 {this.state.openMasters[title] && (
                                                     <ul className="ul-leftnav">
                                                         {MasterTitlesSubs[title].map((subItem: string) => (
-                                                            <li  id={`li${subItem.replace(/\s+/g, '')}`} key={subItem} onClick={(event) => this.onNavItemClick(event)}>
+                                                            <li id={`li${subItem.replace(/\s+/g, '')}`} key={subItem} onClick={(event) => this.onNavItemClick(event)}>
                                                                 <NavLink to={`/${subItem.replace(/\s+/g, '')}`}>
                                                                     <span>{subItem}</span>
                                                                 </NavLink>
@@ -129,50 +162,49 @@ class NavBar extends React.Component<NavBarProps, NavBarState> {
                                             </div>
                                         ))}
                                     </li>
-
                                     {/* Forms Section */}
                                     <li className="liForms mb-1">
-                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e,'Forms')}>
+                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e, 'Forms')}>
                                             <span><FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon>
-                                            Forms
+                                                Forms
                                             </span>
                                             <span className="icon-down">
                                                 <FontAwesomeIcon icon={this.state.openSideBars['Forms'] ? faChevronUp : faChevronDown} />
                                             </span>
                                         </div>
-                                         {this.state.openSideBars['Forms'] && (
-                                                    <ul className="ul-leftnav">
-                                                        {FormAndViewTitles.map((subItem: string) => (
-                                                            <li id={`li${subItem.replace(/\s+/g, '')}Form`} key={subItem+'Form'} onClick={(event) => this.onNavItemClick(event)}>
-                                                                <NavLink to={`/${subItem.replace(/\s+/g, '')}Form`}>
-                                                                    <span>{subItem}</span>
-                                                                </NavLink>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                )}
+                                        {this.state.openSideBars['Forms'] && (
+                                            <ul className="ul-leftnav">
+                                                {FormAndViewTitles.map((subItem: string) => (
+                                                    <li id={`li${subItem.replace(/\s+/g, '')}Form`} key={subItem + 'Form'} onClick={(event) => this.onNavItemClick(event)}>
+                                                        <NavLink to={`/${subItem.replace(/\s+/g, '')}Form`}>
+                                                            <span>{subItem}</span>
+                                                        </NavLink>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
                                     </li>
                                     {/* Views Section */}
-                                    <li className="liViews mb-2">
-                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e,'Views')}>
+                                    <li className="liViews mb-1">
+                                        <div className="sidebar-title" onClick={(e) => this.toggleSideBarItem(e, 'Views')}>
                                             <span><FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
-                                            Views
+                                                Views
                                             </span>
                                             <span className="icon-down">
                                                 <FontAwesomeIcon icon={this.state.openSideBars['Views'] ? faChevronUp : faChevronDown} />
                                             </span>
                                         </div>
-                                         {this.state.openSideBars['Views'] && (
-                                                    <ul className="ul-leftnav">
-                                                        {FormAndViewTitles.map((subItem: string) => (
-                                                            <li id={`li${subItem.replace(/\s+/g, '')}View`} key={subItem+'View'} onClick={(event) => this.onNavItemClick(event)}>
-                                                                <NavLink to={`/${subItem.replace(/\s+/g, '')}View`}>
-                                                                    <span>{subItem}</span>
-                                                                </NavLink>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                )}
+                                        {this.state.openSideBars['Views'] && (
+                                            <ul className="ul-leftnav">
+                                                {FormAndViewTitles.map((subItem: string) => (
+                                                    <li id={`li${subItem.replace(/\s+/g, '')}View`} key={subItem + 'View'} onClick={(event) => this.onNavItemClick(event)}>
+                                                        <NavLink to={`/${subItem.replace(/\s+/g, '')}View`}>
+                                                            <span>{subItem}</span>
+                                                        </NavLink>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
                                     </li>
 
                                 </ul>
